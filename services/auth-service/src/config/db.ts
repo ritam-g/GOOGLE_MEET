385c9fb9638ef2eebcba30c266db9c85734077ff
ritam-g <ritammaty2006@gmail.com>
2026-08-15 22:12:03 +0530

    feat(auth-service): refactor server setup and add basic health check endpoint
    - Updated package.json to use tsx for development.
    - Created server.ts to initialize the Express app and listen on the specified port.
    - Added app.ts to define the Express application and a health check route.
    - Introduced db.ts for Prisma client setup with PostgreSQL adapter.
    - Added env.ts for environment variable management.

---
+++ services/auth-service/src/config/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { DATABASE_URL } from './env.js';
+
+const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
---
+++ services/auth-service/src/config/env.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export const PORT: string = process.env.PORT || '4001';
+export const DATABASE_URL: string = process.env.DATABASE_URL as string;
+export const JWT_ACCESS_SECRET: string = process.env.JWT_ACCESS_SECRET as string;
+export const JWT_REFRESH_SECRET: string = process.env.JWT_REFRESH_SECRET as string;
+export const REDIS_URL: string = process.env.REDIS_URL as string;
