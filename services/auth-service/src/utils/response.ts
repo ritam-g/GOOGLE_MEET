1f4288be34e183669fe93694c4d769cea751b2c6
ritam-g <ritammaty2006@gmail.com>
2026-08-16 22:23:21 +0530

    feat(auth-service): enhance signupController to use successResponse utility and handle email conflict

--- services/auth-service/src/utils/response.ts
+++ services/auth-service/src/utils/response.ts
@@ -3,10 +3,12 @@ import { Response } from "express"
 export const successResponse = (
     res: Response,
     data: object,
-    statusCode: number = 200
+    statusCode: number = 200,
+    message: string = 'Request successful'
 ) => {
     return res.status(statusCode).json({
         success: true,
         data,
+        message
     })
 }
