7cfe99ab83de1d6c7a078d00737a8cfa2b5d0c34
ritam-g <ritammaty2006@gmail.com>
2026-08-25 22:03:07 +0530

    feat(room-service): update environment configuration, add validation middleware, and upgrade dependencies

--- services/room-service/src/config/env.ts
+++ services/room-service/src/config/env.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 
-export const PORT: string = process.env.PORT || '4001';
+export const PORT: string = process.env.PORT || '4003';
 export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 export const JWT_ACCESS_SECRET: string = process.env.JWT_ACCESS_SECRET as string;
-export const JWT_REFRESH_SECRET: string = process.env.JWT_REFRESH_SECRET as string;
-export const REDIS_URL: string = process.env.REDIS_URL as string;
 export const NODE_ENV: string = process.env.NODE_ENV as string;
