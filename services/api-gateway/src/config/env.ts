dd37cfb8647c3f6617d1f9d885e9355d901270d2
ritam-g <ritammaty2006@gmail.com>
2026-08-31 22:26:15 +0530

    feat(api-gateway): add JWT authentication middleware and update dependencies

--- services/api-gateway/src/config/env.ts
+++ services/api-gateway/src/config/env.ts
@@ -6,3 +6,4 @@ export const USER_SERVICE_URL: string = process.env.USER_SERVICE_URL as string;
 export const ROOM_SERVICE_URL: string = process.env.ROOM_SERVICE_URL as string;
 export const FRONTEND_URL: string = process.env.FRONTEND_URL as string;
 export const NODE_ENV: string = process.env.NODE_ENV as string;
+export const JWT_ACCESS_SECRET: string = process.env.JWT_ACCESS_SECRET as string;
---
+++ services/api-gateway/src/middleware/gatewayAuth.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_ACCESS_SECRET } from '../config/env.js';
+import AppError from '../utils/AppError.js';
+
+export function gatewayAuth(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return next(new AppError('No token provided', 401));
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(token, JWT_ACCESS_SECRET) as { userId: string };
+        req.userId = decoded.userId;
+        next()
+    } catch (err) {
+        return next(new AppError('Invalid token', 401));
+    }
+
+}
